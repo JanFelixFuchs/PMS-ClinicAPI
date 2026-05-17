51387c253cbe7a532581faebff37c185c41c46fa
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-05-17 20:24:10 +0300

    add: appointment and appointment protocol status enums

---
+++ Domain/Commons/Enums/AppointmentProtocolStatus.cs
@@ -0,0 +1,8 @@
+namespace Domain.Commons.Enums;
+
+public enum AppointmentProtocolStatus
+{
+    Undealt = 0,
+    Started = 1,
+    Completed = 2
+}
---
+++ Domain/Commons/Enums/AppointmentStatus.cs
@@ -0,0 +1,7 @@
+namespace Domain.Commons.Enums;
+
+public enum AppointmentStatus
+{
+    Planned = 0,
+    Completed = 1
+}
