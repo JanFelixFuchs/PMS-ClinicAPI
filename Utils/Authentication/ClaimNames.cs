a237ceaaeebf13cc706c9941f754767ec3474a37
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-05-20 11:26:10 +0300

    add: utils file for claim names

---
+++ Utils/Authentication/ClaimNames.cs
@@ -0,0 +1,9 @@
+namespace Utils.Authentication;
+
+public static class ClaimNames
+{
+    public const string ClinicId = "clinicId";
+    public const string UserId = "userId";
+
+    public const string PermissionPrefix = "perm:";
+}
