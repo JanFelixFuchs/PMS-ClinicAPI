1b2dfe9d8b8c97d9a696b37756bb342e3431e52d
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-05-25 13:17:22 +0300

    add: application layer log messages

---
+++ Application/Common/Logging/LogMessages.cs
@@ -0,0 +1,21 @@
+namespace Application.Common.Logging;
+
+public static class LogMessages
+{
+    public const string RequestStarted = "Started request {Request}";
+    public const string RequestCompleted = "Completed request {Request}";
+    
+    public const string EntityNotFound = "Failed to find {Entity} with id {Id}";
+    public const string EntitiesNotFound = "Failed to find {Entities} with ids {Ids}";
+    public const string EntityPropertyNotUnique = "Provided {Property} of {Entity} is not unique";
+    public const string EntityPropertyInvalid = "Provided {Property} of {Entity} is invalid";
+    public const string EntityPropertyUnchanged = "Provided {Property} of {Entity} is unchanged";
+    
+    public const string ValidationFailed = "Failed to validate request {Request} with messages {Messages}";
+    
+    public const string MissingMandatoryClaim = "Failed to authorize due to missing mandatory claims {ClinicIdClaim} or {UserIdClaim}";
+    public const string InvalidMandatoryClaim = "Failed to authorize due to invalid mandatory claim {Claim}";
+    public const string InvalidRefreshToken = "Failed to authorize due to invalid or expired refresh token for user with id {UserId}";
+    
+    public const string InvalidPassword = "Failed to authorize due to invalid password for user with id {UserId}";
+}
