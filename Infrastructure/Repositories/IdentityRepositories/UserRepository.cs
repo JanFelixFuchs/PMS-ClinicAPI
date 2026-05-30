42d736975cd7e684f91dc6536c37e1296801ee66
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-05-31 00:20:16 +0300

    add: user repository implementation

---
+++ Infrastructure/Repositories/IdentityRepositories/UserRepository.cs
@@ -0,0 +1,132 @@
+using System.Linq.Expressions;
+using Application.Repositories.IdentityRepositories;
+using Domain.Entities.IdentityEntities;
+using Infrastructure.Common.Exceptions.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.IdentityRepositories;
+
+public class UserRepository(DatabaseContext databaseContext) : IUserRepository
+{
+    public async Task AddAsync(User user, CancellationToken cancellationToken)
+    {
+        try
+        {
+            // Adding user
+            await databaseContext.Users.AddAsync(user, cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseCreateException(nameof(User), exception.Message);
+        }
+    }
+
+    public async Task<ICollection<User>> GetByClinicIdAsync(
+        Guid clinicId, 
+        bool archived,
+        CancellationToken cancellationToken, 
+        params Expression<Func<User, object?>>[] includeProperties)
+    {
+        try
+        {
+            // Initializing query
+            IQueryable<User> query = databaseContext.Users;
+            
+            // Including properties
+            foreach (var property in includeProperties)
+                query = query.Include(property);
+            
+            // Querying users
+            return await query
+                .Where(user => user.ClinicId == clinicId && user.IsArchived == archived)
+                .ToListAsync(cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception 
+            throw new DatabaseReadException(nameof(User), exception.Message);
+        }
+    }
+    
+    public async Task<ICollection<User>> GetByClinicIdAndUserIdsAsync(
+        Guid clinicId, 
+        ICollection<Guid> userIds, 
+        CancellationToken cancellationToken, 
+        params Expression<Func<User, object?>>[] includeProperties)
+    {
+        try
+        {
+            // Initializing query
+            IQueryable<User> query = databaseContext.Users;
+            
+            // Including properties
+            foreach (var property in includeProperties)
+                query = query.Include(property);
+            
+            // Querying users
+            return await query
+                .Where(user => user.ClinicId == clinicId && userIds.Contains(user.Id))
+                .ToListAsync(cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseReadException(nameof(User), exception.Message);
+        }
+    }
+    
+    public async Task<User?> GetByClinicIdAndUserIdAsync(
+        Guid clinicId,
+        Guid userId,
+        CancellationToken cancellationToken,
+        params Expression<Func<User, object?>>[] includeProperties)
+    {
+        try
+        {
+            // Initializing query
+            IQueryable<User> query = databaseContext.Users;
+            
+            // Including properties
+            foreach (var property in includeProperties)
+                query = query.Include(property);
+            
+            // Querying user
+            return await query
+                .Where(user => user.ClinicId == clinicId && user.Id == userId)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseReadException(nameof(User), exception.Message);
+        }
+    }
+
+    public async Task<User?> GetByClinicIdAndNormalizedUsernameAsync(
+        Guid clinicId, 
+        string normalizedUsername, 
+        CancellationToken cancellationToken, 
+        params Expression<Func<User, object?>>[] includeProperties)
+    {
+        try
+        {
+            // Initializing query
+            IQueryable<User> query = databaseContext.Users;
+            
+            // Including properties
+            foreach (var property in includeProperties)
+                query = query.Include(property);
+            
+            // Querying user
+            return await query
+                .Where(user => user.ClinicId == clinicId && user.NormalizedUsername == normalizedUsername)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseReadException(nameof(User), exception.Message);
+        }
+    }
+}
