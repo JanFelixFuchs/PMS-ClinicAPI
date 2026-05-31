f10a31bce459b1c93a3daa8467d92de79fa1e579
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-06-01 01:02:06 +0300

    add: appointment repository implementation

---
+++ Infrastructure/Repositories/AppointmentRepositories/AppointmentRepository.cs
@@ -0,0 +1,113 @@
+using System.Linq.Expressions;
+using Application.Repositories.AppointmentRepositories;
+using Domain.Entities.AppointmentEntities;
+using Infrastructure.Common.Exceptions.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.AppointmentRepositories;
+
+public class AppointmentRepository(DatabaseContext databaseContext) : IAppointmentRepository
+{
+    public async Task AddAsync(Appointment appointment, CancellationToken cancellationToken)
+    {
+        try
+        {
+            // Adding appointment
+            await databaseContext.Appointments.AddAsync(appointment, cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseCreateException(nameof(Appointment), exception.Message);
+        }
+    }
+
+    public async Task<ICollection<Appointment>> GetOverlappingByClinicIdAndDateTimesAsync(
+        Guid clinicId, 
+        DateTime startDateTime, 
+        DateTime endDateTime,
+        CancellationToken cancellationToken, 
+        params Expression<Func<Appointment, object?>>[] includeProperties)
+    {
+        try
+        {
+            // Initializing query
+            IQueryable<Appointment> query = databaseContext.Appointments;
+            
+            // Including properties
+            foreach (var include in includeProperties)
+                query = query.Include(include);
+            
+            // Querying appointments
+            return await query
+                .Where(
+                    appointment => appointment.ClinicId == clinicId && 
+                    appointment.StartTime < endDateTime && 
+                    appointment.EndTime > startDateTime)
+                .ToListAsync(cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseReadException(nameof(Appointment), exception.Message);
+        }
+    }
+    
+    public async Task<ICollection<Appointment>> GetByClinicIdAndDatesAsync(
+        Guid clinicId, 
+        DateOnly startDate, 
+        DateOnly endDate,
+        CancellationToken cancellationToken, 
+        params Expression<Func<Appointment, object?>>[] includeProperties)
+    {
+        try
+        {
+            // Initializing query
+            IQueryable<Appointment> query = databaseContext.Appointments;
+            
+            // Including properties
+            foreach (var include in includeProperties)
+                query = query.Include(include);
+            
+            // Querying appointments
+            return await query
+                .Where(
+                    appointment => appointment.ClinicId == clinicId && 
+                                   appointment.StartTime >= startDate.ToDateTime(TimeOnly.MinValue) && 
+                                   appointment.EndTime <= endDate.ToDateTime(TimeOnly.MaxValue))
+                .ToListAsync(cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseReadException(nameof(Appointment), exception.Message);
+        }
+    }
+    
+    public async Task<Appointment?> GetByClinicIdAndAppointmentIdAsync(
+        Guid clinicId, 
+        Guid appointmentId, 
+        CancellationToken cancellationToken,
+        params Expression<Func<Appointment, object?>>[] includeProperties)
+    {
+        try
+        {
+            // Initializing query
+            IQueryable<Appointment> query = databaseContext.Appointments;
+            
+            // Including properties
+            foreach (var include in includeProperties)
+                query = query.Include(include);
+            
+            // Querying appointment
+            return await query
+                .Where(appointment => appointment.ClinicId == clinicId && appointment.Id == appointmentId)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseReadException(nameof(Appointment), exception.Message);
+        }
+    }
+}
