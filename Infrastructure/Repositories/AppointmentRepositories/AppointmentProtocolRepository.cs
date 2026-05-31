7ca46d8585e2b905b771bf1d12a878a50e58fed0
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-06-01 01:02:01 +0300

    add: appointment protocol repository implementation

---
+++ Infrastructure/Repositories/AppointmentRepositories/AppointmentProtocolRepository.cs
@@ -0,0 +1,51 @@
+using System.Linq.Expressions;
+using Application.Repositories.AppointmentRepositories;
+using Domain.Entities.AppointmentEntities;
+using Infrastructure.Common.Exceptions.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.AppointmentRepositories;
+
+public class AppointmentProtocolRepository(DatabaseContext databaseContext) : IAppointmentProtocolRepository
+{
+    public async Task AddAsync(AppointmentProtocol appointmentProtocol, CancellationToken cancellationToken)
+    {
+        try
+        {
+            // Adding appointment protocols
+            await databaseContext.AppointmentProtocols.AddAsync(appointmentProtocol, cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseCreateException(nameof(AppointmentProtocol), exception.Message);
+        }
+    }
+    
+    public async Task<AppointmentProtocol?> GetByClinicIdAndAppointmentProtocolIdAsync(
+        Guid clinicId, 
+        Guid appointmentProtocolId,
+        CancellationToken cancellationToken, 
+        params Expression<Func<AppointmentProtocol, object>>[] includeProperties)
+    {
+        try
+        {
+            // Initializing query
+            IQueryable<AppointmentProtocol> query = databaseContext.AppointmentProtocols;
+            
+            // Including properties
+            foreach (var property in includeProperties)
+                query = query.Include(property);
+            
+            // Querying appointment protocol
+            return await query
+                .Where(appointmentProtocol => appointmentProtocol.ClinicId == clinicId && appointmentProtocol.Id == appointmentProtocolId)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+        catch (Exception exception)
+        {
+            // Throwing exception
+            throw new DatabaseReadException(nameof(AppointmentProtocol), exception.Message);
+        }
+    }
+}
