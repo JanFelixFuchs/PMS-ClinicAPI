c5303caea9edbfa1fd8792a99ef910c5b11f8110
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-06-06 00:03:38 +0300

    add: global exception handler middleware

---
+++ ClinicAPI/Common/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,50 @@
+using System.Net;
+using Application.Common.Exceptions;
+using PMS_ClinicAPI.Common.Logging;
+using PMS_ClinicAPI.Common.Utils.Returns;
+using Utils.Exceptions.Base;
+
+namespace PMS_ClinicAPI.Common.Middleware;
+
+public class GlobalExceptionHandlerMiddleware(
+    ILogger<GlobalExceptionHandlerMiddleware> logger,
+    RequestDelegate next)
+{
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            // Executing next action
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            // Handling authorization failures without any response body
+            if (exception is AuthorizationFailedException)
+            {
+                logger.LogInformation(LogMessages.EndpointCallFailed, HttpStatusCode.Unauthorized, "");
+                context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                return;
+            }
+            
+            // Creating http result
+            var httpResult = new HttpResult<EmptyPayload>(exception);
+            
+            // Logging exception
+            if (exception is CustomExceptionBase)
+            { 
+                logger.LogInformation(LogMessages.EndpointCallFailed, httpResult.HttpStatusCode, httpResult);
+            }
+            else
+            { 
+                logger.LogCritical(exception, LogMessages.UnexpectedException);   
+            }
+            
+            // Setting response status code
+            context.Response.StatusCode = httpResult.HttpStatusCode;
+            
+            // Setting response body
+            await context.Response.WriteAsJsonAsync(httpResult);
+        }
+    }
+}
