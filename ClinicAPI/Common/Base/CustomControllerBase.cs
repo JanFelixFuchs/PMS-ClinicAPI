6d7c4c561f1c25dccc7e1a2c6ac3be35c50e348f
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-06-07 00:22:57 +0300

    update: custom controller base class for storing refresh token in cookie

--- ClinicAPI/Common/Base/CustomControllerBase.cs
+++ ClinicAPI/Common/Base/CustomControllerBase.cs
@@ -1,6 +1,9 @@
 using System.Net;
+using Application.Common.Exceptions;
+using Infrastructure.Common.Configuration;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using PMS_ClinicAPI.Common.Logging;
 using PMS_ClinicAPI.Common.Utils.Returns;
 
@@ -8,29 +11,41 @@ namespace PMS_ClinicAPI.Common.Base;
 
 public abstract class CustomControllerBase<TController>(
     ILogger<TController> logger,
+    IOptions<CookieSettings> cookieSettings,
+    IOptions<JwtSettings> jwtSettings,
     ISender sender)
     : ControllerBase
 {
-    protected async Task<ActionResult<HttpResult<T>>> Execute<T>(
-        IRequest<T> request,
+    private const string RefreshTokenCookieName = "refreshToken";
+    private const string RefreshTokenCookiePath = "/api/auth/refresh";
+    
+    protected async Task<ActionResult<HttpResult<TPayload>>> Execute<TResult, TPayload>(
+        IRequest<TResult> request,
         HttpStatusCode statusCode,
-        string endpointName)
+        string endpointName,
+        Func<TResult, TPayload> payloadSelector,
+        Action<TResult>? postProcessingAction = null)
     {
         // Logging endpoint execution start
         logger.LogInformation(LogMessages.EndpointCallStarted, endpointName);
         
         // Executing request
         var requestResult = await sender.Send(request);
+        
+        // Invoking post-processing action
+        postProcessingAction?.Invoke(requestResult);
 
         // Logging endpoint execution completion and returning result
-        var httpResult = new HttpResult<T>(statusCode, requestResult);
+        var httpResult = new HttpResult<TPayload>(statusCode, payloadSelector(requestResult));
         logger.LogInformation(LogMessages.EndpointCallSucceeded, endpointName, statusCode);
         return StatusCode(httpResult.HttpStatusCode, httpResult);
     }
     
     protected async Task<ActionResult<HttpResult<EmptyPayload>>> Execute(
         IRequest request,
-        string endpointName)
+        HttpStatusCode statusCode,
+        string endpointName,
+        Action? postProcessingAction = null)
     {
         // Logging endpoint executing start
         logger.LogInformation(LogMessages.EndpointCallStarted, endpointName);
@@ -38,9 +53,46 @@ public abstract class CustomControllerBase<TController>(
         // Executing request
         await sender.Send(request);
 
+        // Invoking post-processing action
+        postProcessingAction?.Invoke();
+        
         // Logging endpoint execution completion and returning result
-        var httpResult = new HttpResult<EmptyPayload>(HttpStatusCode.OK);
-        logger.LogInformation(LogMessages.EndpointCallSucceeded, endpointName, HttpStatusCode.OK);
+        var httpResult = new HttpResult<EmptyPayload>(statusCode);
+        logger.LogInformation(LogMessages.EndpointCallSucceeded, endpointName, statusCode);
         return StatusCode(httpResult.HttpStatusCode, httpResult);
+    }
+
+    protected string GetRefreshTokenCookie()
+    {
+        // Getting and checking refresh token from cookie
+        if (!Request.Cookies.TryGetValue(RefreshTokenCookieName, out var refreshToken))
+            throw new AuthorizationFailedException();
+        
+        // Returning refresh token
+        return refreshToken;
+    }
+    
+    protected void SetRefreshTokenCookie(string refreshToken)
+    {
+        // Setting refresh token cookie
+        Response.Cookies.Append(RefreshTokenCookieName, refreshToken, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = cookieSettings.Value.Secure,
+            SameSite = cookieSettings.Value.SameSiteMode,
+            Expires = DateTimeOffset.UtcNow.AddDays(jwtSettings.Value.RefreshTokenLifetimeInDays),
+            Path = cookieSettings.Value.RestrictPath ? RefreshTokenCookiePath : null
+        });
+    }
+    
+    protected void DeleteRefreshTokenCookie()
+    {
+        Response.Cookies.Delete(RefreshTokenCookieName, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = cookieSettings.Value.Secure,
+            SameSite = cookieSettings.Value.SameSiteMode,
+            Path = cookieSettings.Value.RestrictPath ? RefreshTokenCookiePath : null
+        });
     }
 }
