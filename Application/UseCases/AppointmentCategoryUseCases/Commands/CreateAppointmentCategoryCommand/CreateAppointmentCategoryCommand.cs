e68426ee4bf9178c3b7057541cec48e48d4e09d7
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-06-16 00:09:55 +0300

    add: command for creating an appointment category

---
+++ Application/UseCases/AppointmentCategoryUseCases/Commands/CreateAppointmentCategoryCommand/CreateAppointmentCategoryCommand.cs
@@ -0,0 +1,16 @@
+using Application.Common.Behaviours.RequestContextBehaviour;
+using Application.Common.OutputModels.AppointmentOutputModels;
+using Domain.Entities.IdentityEntities;
+using MediatR;
+
+namespace Application.UseCases.AppointmentCategoryUseCases.Commands.CreateAppointmentCategoryCommand;
+
+public record CreateAppointmentCategoryCommand(
+    string Name,
+    string Abbreviation,
+    string Color)
+    : IRequest<AppointmentCategoryDetailedOutputModel>, IRequireRequestContext
+{
+    public Clinic Clinic { get; set; } = null!;
+    public User User { get; set; } = null!;
+} 
