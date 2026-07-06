e1c36512085504d6d662f51696fac9bb4e8ae849
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-06 22:01:50 +0300

    add: command for updating device category

---
+++ Application/UseCases/DeviceCategoryUseCases/Commands/UpdateDeviceCategoryCommand/UpdateDeviceCategoryCommand.cs
@@ -0,0 +1,18 @@
+using Application.Common.Behaviours.RequestContextBehaviour;
+using Application.Common.OutputModels.DeviceOutputModels;
+using Domain.Entities.IdentityEntities;
+using MediatR;
+
+namespace Application.UseCases.DeviceCategoryUseCases.Commands.UpdateDeviceCategoryCommand;
+
+public record UpdateDeviceCategoryCommand(
+    Guid Id,
+    string Name,
+    string Abbreviation,
+    string Color,
+    ICollection<Guid> DeviceIds) 
+    : IRequest<DeviceCategoryDetailedOutputModel>, IRequireRequestContext
+{
+    public Clinic Clinic { get; set; } = null!;
+    public User User { get; set; } = null!;
+}
