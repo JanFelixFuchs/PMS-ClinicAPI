9ff841d470a438639dd90af71c0ecc5081dc587c
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-11 12:56:03 +0300

    add: handler for reading device

---
+++ Application/UseCases/DeviceUseCases/Queries/ReadDeviceQuery/ReadDeviceQueryHandler.cs
@@ -0,0 +1,41 @@
+using Application.Common.Exceptions;
+using Application.Common.Logging;
+using Application.Common.OutputModels.DeviceOutputModels;
+using Application.Repositories.DeviceRepositories;
+using Domain.Entities.DeviceEntities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.UseCases.DeviceUseCases.Queries.ReadDeviceQuery;
+
+public class ReadDeviceQueryHandler(
+    ILogger<ReadDeviceQueryHandler> logger,
+    IDeviceRepository deviceRepository)
+    : IRequestHandler<ReadDeviceQuery, DeviceDetailedOutputModel>
+{
+    public async Task<DeviceDetailedOutputModel> Handle(ReadDeviceQuery request, CancellationToken cancellationToken)
+    {
+        // Querying and checking device
+        var device = await deviceRepository.GetByClinicIdAndDeviceIdAsync(
+            request.Clinic.Id, 
+            request.Id, 
+            cancellationToken,
+            device => device.DeviceCategories,
+            device => device.Appointments,
+            device => device.AppointmentProtocols,
+            device => device.Results);
+        if (device == null)
+        {
+            logger.LogWarning(LogMessages.EntityNotFound, nameof(device), request.Id);
+            throw new NotFoundException(nameof(Device), request.Id);
+        }
+        
+        // Returning output model
+        return new DeviceDetailedOutputModel(
+            device,
+            device.DeviceCategories,
+            device.Appointments,
+            device.AppointmentProtocols,
+            device.Results);
+    }
+}
