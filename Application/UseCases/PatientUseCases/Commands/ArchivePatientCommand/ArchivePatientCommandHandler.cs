051daae13f6d5b470453d7e748da59c8f096ecd7
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-15 17:52:31 +0300

    add: handler for archiving patient

---
+++ Application/UseCases/PatientUseCases/Commands/ArchivePatientCommand/ArchivePatientCommandHandler.cs
@@ -0,0 +1,47 @@
+using Application.Common.Exceptions;
+using Application.Common.Logging;
+using Application.Common.OutputModels.PatientOutputModels;
+using Application.Common.Transactions;
+using Application.Repositories.PatientRepositories;
+using Domain.Entities.PatientEntities;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.UseCases.PatientUseCases.Commands.ArchivePatientCommand;
+
+public class ArchivePatientCommandHandler(
+    ILogger<ArchivePatientCommandHandler> logger,
+    IPatientRepository patientRepository,
+    IUnitOfWork unitOfWork)
+    : IRequestHandler<ArchivePatientCommand, PatientDetailedOutputModel>
+{
+    public async Task<PatientDetailedOutputModel> Handle(ArchivePatientCommand request, CancellationToken cancellationToken)
+    {
+        return await unitOfWork.ExecuteAsync(async () =>
+        {
+            // Querying and checking patient
+            var patient = await patientRepository.GetByClinicIdAndPatientIdAsync(
+                request.Clinic.Id, 
+                request.Id, 
+                cancellationToken,
+                patient => patient.Appointments,
+                patient => patient.AppointmentProtocols,
+                patient => patient.Results);
+            if (patient == null)
+            {
+                logger.LogWarning(LogMessages.EntityNotFound, nameof(patient), request.Id);
+                throw new NotFoundException(nameof(Patient), request.Id);
+            }
+            
+            // Archiving patient
+            patient.Archive(patient.Appointments, patient.AppointmentProtocols);
+            
+            // Returning output model
+            return new PatientDetailedOutputModel(
+                patient, 
+                patient.Appointments, 
+                patient.AppointmentProtocols, 
+                patient.Results);
+        }, cancellationToken);
+    }
+}
