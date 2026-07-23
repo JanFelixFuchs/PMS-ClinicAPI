ee5c550899d9509c471e7ea33dffa91000aa0389
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-23 23:09:01 +0300

    update: exception for failed authorization after updating custom exception base

--- Utils/Exceptions/CustomExceptions/AuthorizationFailedException.cs
+++ Utils/Exceptions/CustomExceptions/AuthorizationFailedException.cs
@@ -1,7 +1,11 @@
 using System.Net;
 using Utils.Exceptions.Base;
+using Utils.Exceptions.Errors.Types;
 
 namespace Utils.Exceptions.CustomExceptions;
 
-public class AuthorizationFailedException()
-    : CustomExceptionBase("Authorization failed", HttpStatusCode.Unauthorized);
+public class AuthorizationFailedException(string? logMessage = null)
+    : CustomExceptionBase(
+        logMessage ?? "Authorization failed", 
+        HttpStatusCode.Unauthorized,
+        ErrorType.AUTHORIZATION_FAILED);
--- Utils/Exceptions/Errors/Types/ErrorTypes.cs
+++ Utils/Exceptions/Errors/Types/ErrorTypes.cs
@@ -5,6 +5,7 @@ public enum ErrorType
 {
     SUCCESS,
     
-
+    AUTHORIZATION_FAILED,
+    
     INTERNAL_ERROR
 }
