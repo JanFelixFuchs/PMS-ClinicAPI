293605805ea56a684c98b032bbae643c31152dac
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-25 00:34:55 +0300

    add: validator for deleting room category

---
+++ Application/UseCases/RoomCategoryUseCases/Commands/DeleteRoomCategoryCommand/DeleteRoomCategoryCommandValidator.cs
@@ -0,0 +1,12 @@
+using Application.Common.Behaviours.Validation.Rules;
+using FluentValidation;
+
+namespace Application.UseCases.RoomCategoryUseCases.Commands.DeleteRoomCategoryCommand;
+
+public class DeleteRoomCategoryCommandValidator : AbstractValidator<DeleteRoomCategoryCommand>
+{
+    public DeleteRoomCategoryCommandValidator()
+    {
+        RuleFor(command => command.Id).ValidRequiredGuid();
+    }
+}
