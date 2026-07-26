bb615a3a87fe8ca6d4875d91fadafe672c84ce38
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-27 00:34:15 +0300

    update: pipeline enumerable validation rules to include error codes

--- Application/Common/Behaviours/Validation/Rules/EnumerableValidationRules.cs
+++ Application/Common/Behaviours/Validation/Rules/EnumerableValidationRules.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using Utils.Exceptions.Errors.Codes;
 
 namespace Application.Common.Behaviours.Validation.Rules;
 
@@ -7,30 +8,36 @@ public static class EnumerableValidationRules
     public static IRuleBuilderOptions<T, ICollection<TElement>> ValidRequiredCollection<T, TElement>(this IRuleBuilder<T, ICollection<TElement>> ruleBuilder) =>
         ruleBuilder
             .NotNull()
+            .WithState(_ => ErrorCode.MISSING_VALUE)
             .WithMessage("{PropertyName} must be not null");
     
     public static IRuleBuilderOptions<T, ICollection<TElement>> ValidRequiredDuplicateFreeCollection<T, TElement>(this IRuleBuilder<T, ICollection<TElement>> ruleBuilder) =>
         ruleBuilder
             .Must(value => value.Distinct().Count() == value.Count)
+            .WithState(_ => ErrorCode.CONTAINS_DUPLICATE_ELEMENTS)
             .WithMessage("{PropertyName} must not contain duplicate elements");
     
     public static IRuleBuilderOptions<T, ICollection<TElement>> ValidRequiredMinimumCollectionLength<T, TElement>(this IRuleBuilder<T, ICollection<TElement>> ruleBuilder, int minLength = 1) =>
         ruleBuilder
             .Must(value => value.Count >= minLength)
+            .WithState(_ => ErrorCode.MIN_LENGTH_NOT_REACHED)
             .WithMessage($"{{PropertyName}} must be not shorter than {minLength} elements");
     
     public static IRuleBuilderOptions<T, TElement[]> ValidRequiredArray<T, TElement>(this IRuleBuilder<T, TElement[]> ruleBuilder) =>
         ruleBuilder
             .NotNull()
+            .WithState(_ => ErrorCode.MISSING_VALUE)
             .WithMessage("{PropertyName} must be not null or empty");
     
     public static IRuleBuilderOptions<T, TElement[]> ValidRequiredMinimumArrayLength<T, TElement>(this IRuleBuilder<T, TElement[]> ruleBuilder, int minLength = 1) =>
         ruleBuilder
             .Must(value => value.Length >= minLength)
+            .WithState(_ => ErrorCode.MIN_LENGTH_NOT_REACHED)
             .WithMessage($"{{PropertyName}} must be not shorter than {minLength} elements");
     
     public static IRuleBuilderOptions<T, TElement[]> ValidRequiredMaximumArrayLength<T, TElement>(this IRuleBuilder<T, TElement[]> ruleBuilder, int maxLength) =>
         ruleBuilder
             .Must(value => value.Length <= maxLength)
+            .WithState(_ => ErrorCode.MAX_LENGTH_EXCEEDED)
             .WithMessage($"{{PropertyName}} must be not longer than {maxLength} elements");
 }
