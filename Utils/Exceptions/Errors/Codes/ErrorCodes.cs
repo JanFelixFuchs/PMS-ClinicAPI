1df59018b0d2b764fa7ddddb4cbcc90d92ea3cf4
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-26 14:23:32 +0300

    add: missing error code for an invalid input model format

--- Utils/Exceptions/Errors/Codes/ErrorCodes.cs
+++ Utils/Exceptions/Errors/Codes/ErrorCodes.cs
@@ -3,6 +3,7 @@ namespace Utils.Exceptions.Errors.Codes;
 // Resharper disable InconsistentNaming
 public enum ErrorCode
 {
+    INVALID_INPUT_MODEL_FORMAT,
     
     INCORRECT_VALUE,
     VALUE_ALREADY_IN_USE,
