57f414c4496d0815e2d919ae176cf54966d98d00
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-27 00:32:23 +0300

    update: pipeline date time validation rules to include error codes

--- Application/Common/Behaviours/Validation/Rules/DateTimeValidationRules.cs
+++ Application/Common/Behaviours/Validation/Rules/DateTimeValidationRules.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using Utils.Exceptions.Errors.Codes;
 
 namespace Application.Common.Behaviours.Validation.Rules;
 
@@ -7,35 +8,42 @@ public static class DateTimeValidationRules
     public static IRuleBuilderOptions<T, DateTime> ValidRequiredDateTime<T>(this IRuleBuilder<T, DateTime> ruleBuilder) =>
         ruleBuilder
             .NotEmpty()
+            .WithState(_ => ErrorCode.MISSING_VALUE)
             .WithMessage("{PropertyName} must be not empty");
     
     public static IRuleBuilderOptions<T, DateTime> ValidRequiredPastDateTime<T>(this IRuleBuilder<T, DateTime> ruleBuilder) =>
         ruleBuilder
             .LessThanOrEqualTo(_ => DateTime.UtcNow)
+            .WithState(_ => ErrorCode.DATETIME_NOT_IN_PAST)
             .WithMessage("{PropertyName} must be in the past");
     
     public static IRuleBuilderOptions<T, DateTime> ValidRequiredBeforeDateTime<T>(this IRuleBuilder<T, DateTime> ruleBuilder, Func<T, DateTime> maxDateTimeSelector) =>
         ruleBuilder
             .Must((request, dateTime) => dateTime <= maxDateTimeSelector(request))
+            .WithState(_ => ErrorCode.DATETIME_OUT_OF_RANGE)
             .WithMessage("{PropertyName} must be before than or equal to to the maximum allowed date time");
     
     public static IRuleBuilderOptions<T, DateTime> ValidRequiredFutureDateTime<T>(this IRuleBuilder<T, DateTime> ruleBuilder) =>
         ruleBuilder
             .GreaterThanOrEqualTo(_ => DateTime.UtcNow)
+            .WithState(_ => ErrorCode.DATETIME_NOT_IN_FUTURE)
             .WithMessage("{PropertyName} must be in the future");
     
     public static IRuleBuilderOptions<T, DateTime> ValidRequiredAfterDateTime<T>(this IRuleBuilder<T, DateTime> ruleBuilder, Func<T, DateTime> minDateTimeSelector) =>
         ruleBuilder
             .Must((request, dateTime) => dateTime >= minDateTimeSelector(request))
+            .WithState(_ => ErrorCode.DATETIME_OUT_OF_RANGE)
             .WithMessage("{PropertyName} must be after than or equal to to the minimum allowed date time");
     
     public static IRuleBuilderOptions<T, DateTime?> ValidOptionalDateTime<T>(this IRuleBuilder<T, DateTime?> ruleBuilder) =>
         ruleBuilder
             .Must(value => value == null || value != default(DateTime))
+            .WithState(_ => ErrorCode.EMPTY_VALUE)
             .WithMessage("{PropertyName} must be null or not empty");
     
     public static IRuleBuilderOptions<T, DateTime?> ValidOptionalPastDateTime<T>(this IRuleBuilder<T, DateTime?> ruleBuilder) =>
         ruleBuilder
             .Must(value => value == null || value <= DateTime.UtcNow)
+            .WithState(_ => ErrorCode.DATETIME_NOT_IN_PAST)
             .WithMessage("{PropertyName} must be null or in the past");
 }
