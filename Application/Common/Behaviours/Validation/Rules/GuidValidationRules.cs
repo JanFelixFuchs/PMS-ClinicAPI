84bbe05cd3dcbccf38346f208af733e8abf996d0
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-27 00:35:24 +0300

    update: pipeline guid validation rules to include error codes

--- Application/Common/Behaviours/Validation/Rules/GuidValidationRules.cs
+++ Application/Common/Behaviours/Validation/Rules/GuidValidationRules.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using Utils.Exceptions.Errors.Codes;
 
 namespace Application.Common.Behaviours.Validation.Rules;
 
@@ -7,10 +8,12 @@ public static class GuidValidationRules
     public static IRuleBuilderOptions<T, Guid> ValidRequiredGuid<T>(this IRuleBuilder<T, Guid> ruleBuilder) =>
         ruleBuilder
             .NotEmpty()
+            .WithState(_ => ErrorCode.MISSING_VALUE)
             .WithMessage("{PropertyName} must be not empty");
     
     public static IRuleBuilderOptions<T, Guid?> ValidOptionalGuid<T>(this IRuleBuilder<T, Guid?> ruleBuilder) =>
         ruleBuilder
             .Must(value => value == null || value != Guid.Empty)
+            .WithState(_ => ErrorCode.EMPTY_VALUE)
             .WithMessage("{PropertyName} must be null or not empty");
 }
