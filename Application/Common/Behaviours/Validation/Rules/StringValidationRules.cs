bbb902f7fd6e38b6573deb372ce0327a499ffcd7
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-27 00:36:03 +0300

    update: pipeline string validation rules to include error codes

--- Application/Common/Behaviours/Validation/Rules/StringValidationRules.cs
+++ Application/Common/Behaviours/Validation/Rules/StringValidationRules.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using Utils.Exceptions.Errors.Codes;
 
 namespace Application.Common.Behaviours.Validation.Rules;
 
@@ -7,32 +8,38 @@ public static class StringValidationRules
     public static IRuleBuilderOptions<T, string> ValidRequiredString<T>(this IRuleBuilder<T, string> ruleBuilder) =>
         ruleBuilder
             .Must(value => !string.IsNullOrWhiteSpace(value))
+            .WithState(_ => ErrorCode.MISSING_VALUE)
             .WithMessage("{PropertyName} must be not null, empty or whitespace");
 
     public static IRuleBuilderOptions<T, string?> ValidOptionalString<T>(this IRuleBuilder<T, string?> ruleBuilder) =>
         ruleBuilder
             .Must(value => value == null || !string.IsNullOrWhiteSpace(value))
+            .WithState(_ => ErrorCode.EMPTY_VALUE)
             .WithMessage("{PropertyName} must be null, or not empty or whitespace");
 
     public static IRuleBuilderOptions<T, string> ValidRequiredMaximumStringLength<T>(
         this IRuleBuilder<T, string> ruleBuilder, int maxLength) =>
         ruleBuilder
             .MaximumLength(maxLength)
+            .WithState(_ => ErrorCode.MAX_LENGTH_EXCEEDED)
             .WithMessage("{PropertyName} must be not longer than {MaxLength} characters");
 
     public static IRuleBuilderOptions<T, string?> ValidOptionalMaximumStringLength<T>(
         this IRuleBuilder<T, string?> ruleBuilder, int maxLength) =>
         ruleBuilder
             .Must(value => value == null || value.Length <= maxLength)
+            .WithState(_ => ErrorCode.MAX_LENGTH_EXCEEDED)
             .WithMessage($"{{PropertyName}} must be null or not longer than {maxLength} characters");
     
     public static IRuleBuilderOptions<T, string> ValidRequiredRegex<T>(this IRuleBuilder<T, string> ruleBuilder, string regex) =>
         ruleBuilder
             .Matches(regex)
+            .WithState(_ => ErrorCode.PATTERN_MISMATCH)
             .WithMessage("{PropertyName} must match the required pattern");
     
     public static IRuleBuilderOptions<T, string> ValidRequiredRegex<T>(this IRuleBuilder<T, string> ruleBuilder, Func<T, string> regexSelector) =>
         ruleBuilder
             .Matches(regexSelector)
+            .WithState(_ => ErrorCode.PATTERN_MISMATCH)
             .WithMessage("{PropertyName} must match the required pattern");
 }
