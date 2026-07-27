583d989afe08f35720d7e25e5ef1ee267fc1018f
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-07-27 23:12:46 +0300

    update: validation exception accordingly to updated custom exception base

--- Utils/Exceptions/CustomExceptions/ValidationException.cs
+++ Utils/Exceptions/CustomExceptions/ValidationException.cs
@@ -1,7 +1,15 @@
 using System.Net;
 using Utils.Exceptions.Base;
+using Utils.Exceptions.Errors.Field;
+using Utils.Exceptions.Errors.Types;
 
 namespace Utils.Exceptions.CustomExceptions;
 
-public class ValidationException(ICollection<string> errors)
-    : CustomExceptionBase("Validation failed", HttpStatusCode.UnprocessableEntity, errors);
+public class ValidationException(
+    string logMessage,
+    ICollection<FieldError> fieldErrors)
+    : CustomExceptionBase(
+        logMessage,
+        HttpStatusCode.UnprocessableEntity, 
+        ErrorType.VALIDATION_ERROR, 
+        fieldErrors);
