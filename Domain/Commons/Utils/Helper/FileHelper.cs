917d798e11e2a2c5e17d74e67f088ad1d1e09e67
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-08-03 10:58:49 +0300

    update: file helper to use field error

--- Domain/Commons/Utils/Helper/FileHelper.cs
+++ Domain/Commons/Utils/Helper/FileHelper.cs
@@ -1,5 +1,7 @@
 using Domain.Commons.Enums;
 using Utils.Exceptions.CustomExceptions;
+using Utils.Exceptions.Errors.Codes;
+using Utils.Exceptions.Errors.Field;
 
 namespace Domain.Commons.Utils.Helper;
 
@@ -12,7 +14,9 @@ public static class FileHelper
             [0x25, 0x50, 0x44, 0x46, ..] => AppendixContentType.Pdf,
             [0xFF, 0xD8, 0xFF, ..] => AppendixContentType.Jpeg,
             [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, ..] => AppendixContentType.Png,
-            _ => throw new ValidationException([$"{propertyName} is of an unsupported content type"])
+            _ => throw new ValidationException(
+                $"{propertyName} must be of a supported content type", 
+                [new FieldError(propertyName, ErrorCode.UNSUPPORTED_FILE_TYPE)])
         };
     }
 }
