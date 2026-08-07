2e121b091cc3cb3d1614e5532454f2634400476d
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-08-07 15:42:47 +0300

    add: validator for creating room

---
+++ Application/UseCases/RoomUseCases/Commands/CreateRoomCommand/CreateRoomCommandValidator.cs
@@ -0,0 +1,36 @@
+using Application.Common.Behaviours.Validation.Rules;
+using Domain.Commons.Utils.Constants;
+using FluentValidation;
+
+namespace Application.UseCases.RoomUseCases.Commands.CreateRoomCommand;
+
+public class CreateRoomCommandValidator : AbstractValidator<CreateRoomCommand>
+{
+    public CreateRoomCommandValidator()
+    {
+        RuleFor(command => command.Name)
+            .ValidRequiredString()
+            .ValidRequiredMaximumStringLength(Lengths.RoomName);
+
+        RuleFor(command => command.Abbreviation)
+            .ValidRequiredString()
+            .ValidRequiredMaximumStringLength(Lengths.Abbreviation);
+        
+        RuleFor(command => command.RoomCategoryIds)
+            .ValidRequiredCollection()
+            .ValidRequiredDuplicateFreeCollection();
+        RuleForEach(command => command.RoomCategoryIds).ValidRequiredGuid();
+        
+        RuleFor(command => command.RoomNumber)
+            .ValidOptionalString()
+            .ValidOptionalMaximumStringLength(Lengths.RoomNumber);
+
+        RuleFor(command => command.Floor)
+            .ValidOptionalString()
+            .ValidOptionalMaximumStringLength(Lengths.Floor);
+
+        RuleFor(command => command.Building)
+            .ValidOptionalString()
+            .ValidOptionalMaximumStringLength(Lengths.Building);
+    }
+}
