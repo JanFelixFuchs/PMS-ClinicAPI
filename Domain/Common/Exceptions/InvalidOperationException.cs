e5edde56986d6881f47a5d259170f51e724222c8
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-09-15 19:08:11 +0300

    update: add optional field errors collection to invalid operation exception

--- Domain/Common/Exceptions/InvalidOperationException.cs
+++ Domain/Common/Exceptions/InvalidOperationException.cs
@@ -1,11 +1,22 @@
 using System.Net;
 using Utils.Exceptions.Base;
+using Utils.Exceptions.Errors.Field;
 using Utils.Exceptions.Errors.Types;
 
 namespace Domain.Common.Exceptions;
 
-public class InvalidOperationException(string logMessage)
-    : CustomExceptionBase(
-        logMessage, 
-        HttpStatusCode.Conflict,
-        ErrorType.INVALID_OPERATION);
+public class InvalidOperationException : CustomExceptionBase
+{ 
+    public InvalidOperationException(string logMessage)
+        : base(
+            logMessage, 
+            HttpStatusCode.Conflict,
+            ErrorType.INVALID_OPERATION) { }
+    
+    public InvalidOperationException(string logMessage, ICollection<FieldError> fieldErrors)
+        : base(
+            logMessage, 
+            HttpStatusCode.Conflict,
+            ErrorType.INVALID_OPERATION,
+            fieldErrors) { }
+}
