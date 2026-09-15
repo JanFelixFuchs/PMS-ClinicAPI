cce9ddd2055b86dae5b03c6fb07618a3ae03d431
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-09-15 19:09:46 +0300

    update: extend invariant validation result to take field and error code information

--- Domain/Common/Utils/InvariantValidation/InvariantValidationResult.cs
+++ Domain/Common/Utils/InvariantValidation/InvariantValidationResult.cs
@@ -1,5 +1,9 @@
+using Utils.Exceptions.Errors.Codes;
+
 namespace Domain.Common.Utils.InvariantValidation;
 
 public record InvariantValidationResult(
     bool IsValid,
+    string Field,
+    ErrorCode ErrorCode,
     string ValidationMessage);
