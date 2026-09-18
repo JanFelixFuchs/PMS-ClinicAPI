17145e532d3fbe9ff6861c8207eb01785c2dc64f
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-09-18 19:38:28 +0300

    add: valid zip code and phone number values to test constants

---
+++ TestUtils/Constants/TestConstants.cs
@@ -0,0 +1,11 @@
+namespace TestUtils.Constants;
+
+public static class TestConstants
+{
+    // Valid values
+    public const string ValidGermanZipCode = "12345";
+    public const string ValidFinnishZipCode = "12345";
+    
+    public const string ValidGermanPhoneNumber = "+49123456789";
+    public const string ValidFinnishPhoneNumber = "+358123456789";
+}
