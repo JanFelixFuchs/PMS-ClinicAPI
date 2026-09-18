15f50c5f4baa720cf2017b374b20566895ee2cba
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-09-18 19:40:10 +0300

    add: test constant for current date time default value

--- TestUtils/Constants/TestConstants.cs
+++ TestUtils/Constants/TestConstants.cs
@@ -1,7 +1,12 @@
+using Domain.Common.Enums;
 namespace TestUtils.Constants;
 
 public static class TestConstants
 {
+    // Default current date time
+    public static readonly DateTime DefaultCurrentDateTime = new(2026, 1, 1, 12, 0, 0);
+    
+    
     // Valid values
     public const string ValidGermanZipCode = "12345";
     public const string ValidFinnishZipCode = "12345";
