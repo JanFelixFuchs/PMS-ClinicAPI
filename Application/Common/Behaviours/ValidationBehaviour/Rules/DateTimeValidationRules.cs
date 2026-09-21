87678102f0485fe60a53fd811ec9d720873f13e9
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-09-22 00:09:36 +0300

    update: use newly introduced current date time parameter in command and query validation rules

--- Application/Common/Behaviours/ValidationBehaviour/Rules/DateTimeValidationRules.cs
+++ Application/Common/Behaviours/ValidationBehaviour/Rules/DateTimeValidationRules.cs
@@ -11,9 +11,9 @@ public static class DateTimeValidationRules
             .WithState(_ => ErrorCode.MISSING_VALUE)
             .WithMessage("{PropertyName} must be not empty");
     
-    public static IRuleBuilderOptions<T, DateTime> ValidRequiredPastDateTime<T>(this IRuleBuilder<T, DateTime> ruleBuilder) =>
+    public static IRuleBuilderOptions<T, DateTime> ValidRequiredPastDateTime<T>(this IRuleBuilder<T, DateTime> ruleBuilder, DateTime currentDateTime) =>
         ruleBuilder
-            .LessThanOrEqualTo(_ => DateTime.UtcNow)
+            .LessThanOrEqualTo(_ => currentDateTime)
             .WithState(_ => ErrorCode.DATETIME_NOT_IN_PAST)
             .WithMessage("{PropertyName} must be in the past");
     
@@ -23,9 +23,9 @@ public static class DateTimeValidationRules
             .WithState(_ => ErrorCode.DATETIME_OUT_OF_RANGE)
             .WithMessage("{PropertyName} must be before than or equal to to the maximum allowed date time");
     
-    public static IRuleBuilderOptions<T, DateTime> ValidRequiredFutureDateTime<T>(this IRuleBuilder<T, DateTime> ruleBuilder) =>
+    public static IRuleBuilderOptions<T, DateTime> ValidRequiredFutureDateTime<T>(this IRuleBuilder<T, DateTime> ruleBuilder, DateTime currentDateTime) =>
         ruleBuilder
-            .GreaterThanOrEqualTo(_ => DateTime.UtcNow)
+            .GreaterThanOrEqualTo(_ => currentDateTime)
             .WithState(_ => ErrorCode.DATETIME_NOT_IN_FUTURE)
             .WithMessage("{PropertyName} must be in the future");
     
@@ -41,9 +41,9 @@ public static class DateTimeValidationRules
             .WithState(_ => ErrorCode.EMPTY_VALUE)
             .WithMessage("{PropertyName} must be null or not empty");
     
-    public static IRuleBuilderOptions<T, DateTime?> ValidOptionalPastDateTime<T>(this IRuleBuilder<T, DateTime?> ruleBuilder) =>
+    public static IRuleBuilderOptions<T, DateTime?> ValidOptionalPastDateTime<T>(this IRuleBuilder<T, DateTime?> ruleBuilder, DateTime currentDateTime) =>
         ruleBuilder
-            .Must(value => value == null || value <= DateTime.UtcNow)
+            .Must(value => value == null || value <= currentDateTime)
             .WithState(_ => ErrorCode.DATETIME_NOT_IN_PAST)
             .WithMessage("{PropertyName} must be null or in the past");
 }
