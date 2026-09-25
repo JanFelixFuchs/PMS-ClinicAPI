0c1a0604f5d7e0e6a1e7c9440ee3abb27a62cef7
janfelixfuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-09-26 00:12:54 +0300

    fix: correct typo in names of authentication output models

--- Application/Common/OutputModels/IdentityOutputModels/AuthOutputModels.cs
+++ Application/Common/OutputModels/IdentityOutputModels/AuthOutputModels.cs
@@ -4,13 +4,13 @@ using Domain.Entities.IdentityEntities;
 
 namespace Application.Common.OutputModels.IdentityOutputModels;
 
-public class RegisterClinicOutputModels(Clinic clinic, User user, Role role, Clinician? clinician, string accessToken)
+public class RegisterClinicOutputModel(Clinic clinic, User user, Role role, Clinician? clinician, string accessToken)
     : SessionOutputModel(clinic, user, role, clinician, accessToken);
 
-public class LoginUserOutputModels(Clinic clinic, User user, Role role, Clinician? clinician, string accessToken)
+public class LoginUserOutputModel(Clinic clinic, User user, Role role, Clinician? clinician, string accessToken)
     : SessionOutputModel(clinic, user, role, clinician, accessToken);
 
-public class RefreshTokensOutputModels(Clinic clinic, User user, Role role, Clinician? clinician, string accessToken)
+public class RefreshTokensOutputModel(Clinic clinic, User user, Role role, Clinician? clinician, string accessToken)
     : SessionOutputModel(clinic, user, role, clinician, accessToken);
 
 public class UpdatePasswordOutputModel(string accessToken)
